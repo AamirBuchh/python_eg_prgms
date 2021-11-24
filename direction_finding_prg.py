7027c026b2f24a37131da74e711dd2f38fa5f27b
Aamir <buchh1aamir@gmail.com>
2021-11-24 23:49:09 +0530

    made some minor changes to print function in line 8

--- direction_finding_prg.py
+++ direction_finding_prg.py
@@ -5,7 +5,7 @@
 # eg(with north as initial direction)
 # input --> LLR   (in UPPER case only)
 # output --> W     (i.e. west)
-print("code find the direction North(N),South(S),West(W) and East(E) with intial direction as North(N) signified by variable in_dir we provide direcion from as Left->L and Right->R from variable ins_g to go left or right eg(with north as initial direction) input --> LLR   (in UPPER case only) output --> W     (i.e. west)")
+print("code find the direction North(N),South(S),West(W) and East(E) with intial direction\n as North(N) signified by variable in_dir we provide direcion from as Left->L\n and Right->R from variable ins_g to go left or right eg(with north as initial direction)\n input --> LLR   (in UPPER case only) output --> W     (i.e. west)\n")
 ins_g = input("Enter the code : ")
 in_dir = "N"
 lst_ins = list(ins_g)
